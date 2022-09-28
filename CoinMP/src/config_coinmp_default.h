666995a20d09a2feff70eed148a009603e7e6483
Deepin Developer <deepin-dev@deepin.org>
2022-09-28 13:30:46 +0800

    feat: Init commit
    init coinmp
    
    Log:

---
+++ CoinMP/src/config_coinmp_default.h
@@ -0,0 +1,17 @@
+
+/***************************************************************************/
+/*           HERE DEFINE THE PROJECT SPECIFIC PUBLIC MACROS                */
+/*    These are only in effect in a setting that doesn't use configure     */
+/***************************************************************************/
+
+/* Version number of project */
+#define COINMP_VERSION "1.8.3"
+
+/* Major Version number of project */
+#define COINMP_VERSION_MAJOR 1
+
+/* Minor Version number of project */
+#define COINMP_VERSION_MINOR 8
+
+/* Release Version number of project */
+#define COINMP_VERSION_RELEASE 3
